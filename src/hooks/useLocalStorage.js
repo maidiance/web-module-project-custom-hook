e3978f6e8693747a0e421995d30da8ce8bec97cf
Winifred Lu <winnie.lu@comcast.net>
2022-01-12 14:42:23 -0500

    useLocalStorage built from GP guidance

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+    const [value, setValue] = useState(() => {
+        if(localStorage.getItem(key)) {
+            return(JSON.parse(localStorage.getItem(key)))
+        } else {
+            localStorage.setItem(key, JSON.stringify(initialValue));
+            return(initialValue);
+        }
+    });
+    const setStoredValue = (newValue) => {
+        localStorage.setItem(key, JSON.stringify(newValue));
+        setValue(newValue);
+    }
+    return [value, setStoredValue];
+}
+export default useLocalStorage;
