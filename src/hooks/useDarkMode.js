a8a8d21b8c44646184370c54d731c30975574321
Winifred Lu <winnie.lu@comcast.net>
2022-01-12 14:36:20 -0500

    build useDarkMode hook basics complete

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    return [darkMode, setDarkMode];
+}
+
+export default useDarkMode;
